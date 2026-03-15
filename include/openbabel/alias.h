3153bbe220136ef08aa2677d687b84fff0d099af
Thaison Nguyen <mail@thaison-nguyen.de>
2026-03-15 23:51:59 +0100

    Add shared_ptr compatibility header for C++17 fuzz builds

--- include/openbabel/alias.h
+++ include/openbabel/alias.h
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 #ifndef OB_ALIAS_H
 #define OB_ALIAS_H
 
-#include <memory>
+#include <openbabel/shared_ptr.h>
 #include <vector>
 #include <openbabel/generic.h>
 
---
+++ include/openbabel/shared_ptr.h
@@ -0,0 +1,29 @@
+/**********************************************************************
+shared_ptr.h - Compatibility wrapper for shared_ptr support.
+
+This file is part of the Open Babel project.
+For more information, see <http://openbabel.org/>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2.
+***********************************************************************/
+
+#ifndef OB_SHARED_PTR_H
+#define OB_SHARED_PTR_H
+
+#if __cplusplus >= 201103L || defined(_MSC_VER)
+  #include <memory>
+#else
+  #include <tr1/memory>
+namespace std
+{
+  using tr1::shared_ptr;
+  using tr1::weak_ptr;
+  using tr1::enable_shared_from_this;
+  using tr1::static_pointer_cast;
+  using tr1::dynamic_pointer_cast;
+  using tr1::const_pointer_cast;
+}
+#endif
+
+#endif // OB_SHARED_PTR_H
